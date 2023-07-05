3eeed2642c2431d631046f5664a21f49f6fc9cb0
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-07-05 21:14:32 +0300

    Add relation to db models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -10,5 +10,9 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
+    Pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     Lat = models.FloatField()
     Lon = models.FloatField()
+
+    def __str__(self):
+        return f'{self.Pokemon} находится в точке {self.Lat}-{self.Lon}'
